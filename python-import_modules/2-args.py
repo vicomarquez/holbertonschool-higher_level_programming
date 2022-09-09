99133d924ba54c5983686c74e995a83515e8ca8b
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-09 15:50:27 -0700

    fix pep8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(a))
     for i in range(a):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
