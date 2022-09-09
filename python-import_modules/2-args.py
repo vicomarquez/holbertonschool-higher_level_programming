19642daf3da42a643a8012ce00daf594a6f36cd8
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-09 15:49:31 -0700

    fix error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
+    a = len(sys.argv) - 1
+
+    if a == 1:
+        print("1 argument:")
+    elif a == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(a))
+    for i in range(a):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
