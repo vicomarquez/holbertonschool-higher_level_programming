a885b0e224e8c9949ce0ca16c54191cb91b3a075
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-09 19:16:57 -0700

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,13 +2,9 @@
 if __name__ == "__main__":
     import sys
 
-    a = len(sys.argv) - 1
+    a = len(sys.argv)
+    result = 0
 
-    if a == 1:
-        print("1 argument:")
-    elif a == 0:
-        print("0 arguments.")
-    else:
-        print("{} arguments:".format(a))
-    for i in range(a):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(1, a):
+        result = result + int(sys.argv[i])
+    print("{}".format(result))
