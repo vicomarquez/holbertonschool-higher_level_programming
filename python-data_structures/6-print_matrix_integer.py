5db7c54b6b766b0eac5868e41e617977ab4a925f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-17 03:17:04 -0700

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3i
+
+def print_matrix_integer(matrix=[[]]):
+
+    for i in range(len(matrix)):
+        len_2 = len(matrix[i])
+        for y in range(len_2):
+            if y != len_2 - 1:
+                end = ' '
+            else:
+                end = ''
+            print("{:d}".format(matrix[i][y]), end=end)
+        print("$")
