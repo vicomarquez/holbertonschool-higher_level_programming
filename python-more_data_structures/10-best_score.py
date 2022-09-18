85b0019003bacbe6f8365142928a793842008b8e
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-18 15:52:36 -0700

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    i = list(a_dictionary.values())
+    x = list(a_dictionary.keys())
+
+    return (x[i.index(max(i))])
--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def multiply_list_map(my_list=[], number=0):
