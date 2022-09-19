a0cf4110d9bef6a55c67f4c3781e5311249a7410
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-18 17:23:43 -0700

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,41 @@
 #!/usr/bin/python3
+def get_symbol(i):
+        if (i == 'I'):
+            return 1
+        if (i == 'V'):
+            return 5
+        if (i == 'X'):
+            return 10
+        if (i == 'L'):
+            return 50
+        if (i == 'C'):
+            return 100
+        if (i == 'D'):
+            return 500
+        if (i == 'M'):
+            return 1000
+        return -1
+
+
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total = 0
+    x = 0
+    y = len(roman_string)
+
+    while (x < y):
+        symbol1 = get_symbol(roman_string[x])
+        if (x + 1 < y):
+            symbol2 = get_symbol(roman_string[x + 1])
+
+            if (symbol1 >= symbol2):
+                total = total + symbol1
+                x = x + 1
+            else:
+                total = total + symbol2 - symbol1
+                x = x + 2
+        else:
+            total = total + symbol1
+            x = x + 1
+    return total
