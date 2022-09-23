dbeacf8a2d6d317c8fbcaec15e41587b5026a408
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-23 15:44:29 -0700

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python excercise 1
+"""
+
+
+class Square:
+    """
+    size
+    """
+    def __init__(self, size=0):
+        """
+        size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
