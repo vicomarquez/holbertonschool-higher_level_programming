aea314d36a720d51721ce40c836ef3e80b109bb7
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-22 19:31:35 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python excercise 1
+"""
+
+
+class Square:
+    """
+    size
+    """
+    def __init__(self, size):
+        """
+        size
+        """
+        self.__size = size
