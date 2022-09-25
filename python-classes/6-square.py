1e631ee322aa48c0ed5426ca90daa695eb5b549e
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-24 19:11:17 -0700

    fix error

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -30,14 +30,13 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     @property
     def position(self):
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple:
+        if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -50,7 +49,7 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print ()
+            print()
 
         for i in range(self.__position[1]):
             print("")
