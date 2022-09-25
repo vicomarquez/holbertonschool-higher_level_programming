d728b5755d4e2606382816a43123ececedbff79f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-25 04:31:43 -0700

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
