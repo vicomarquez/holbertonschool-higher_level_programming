667421fe8a0f1d9b733bc0b5bd8c6c08fecfaf1f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-27 11:08:02 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-module
+"""
+
+
+class BaseGeometry:
+    """
+    empty class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
