485690554433a46a86741e624e327fa0d921b363
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-28 03:43:34 -0700

    tasks

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+7-module
+"""
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class BaseGeometry:
+    """
+    empty class BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
