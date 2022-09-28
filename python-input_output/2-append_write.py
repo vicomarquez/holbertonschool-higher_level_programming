a09d304faf7910394fcd756f18af3f0a828744f5
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-28 16:26:19 -0700

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+2-module
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
