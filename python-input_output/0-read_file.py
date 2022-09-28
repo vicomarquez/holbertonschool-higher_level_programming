ed93afb0a714202fcb9b9be1d9109dc0eaba8de4
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-28 15:59:08 -0700

    fix module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
