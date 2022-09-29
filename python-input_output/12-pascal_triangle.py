e68b451da7296baff25d8a5a621a370e344bd7eb
vicomarquez <vicomarquezmuniz@gmail.com>
2022-09-28 19:14:07 -0700

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,6 +6,19 @@
 
 def pascal_triangle(n):
     """
-    returns the dictionary description with simple data structure
+    returns a list of lists of integers representing the Pascal’s triangle
     """
-    return obj.__dict__
+    new_2 = []
+    row = []
+    if n <= 0:
+        return new_2
+    new_2 = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        if i > 0:
+            for x in range (1, i):
+                row.append(new_2[i - 1][x - 1] + new_2[i -1][x])
+            row.append(1)
+        new_2.append(row)
+    return new_2
