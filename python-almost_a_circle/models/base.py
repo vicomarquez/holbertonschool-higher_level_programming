88127aea9d4ccfeb5691fc494a329a672e6b2e57
root <vicomarquezmu@gmail.com>
2022-10-03 14:06:27 -0700

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 1-module"""
+
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
+
