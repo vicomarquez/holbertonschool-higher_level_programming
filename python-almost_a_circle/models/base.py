e26011f661394988ab4535c4b7994bc7eb9c030d
root <vicomarquezmu@gmail.com>
2022-10-03 14:15:21 -0700

    fix pep8wq

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,9 +6,9 @@ class Base:
     """ base """
     __nb_objects = 0
     def __init__(self, id=None):
-        if id != None:
+
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
-
