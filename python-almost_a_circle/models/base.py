472d14df489f8124e5b837d50df24590a832a5d1
vicomarquez <vicomarquezmuniz@gmail.com>
2022-10-05 08:58:01 -0700

    tasks 16 and 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,22 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        new_list = []
+        if list_objs is None:
+            return new_list
+
+        for i in list_objs:
+            new_list.append(i.to_dictionary())
+
+        with open(cls.__name__ + ".json", "w", encoding="utf=8") as f:
+            f.write(cls.to_json_string(new_list))
+
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)
