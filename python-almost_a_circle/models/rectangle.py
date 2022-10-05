03bf5b995de28a521d694f267de4767fe74fd844
vicomarquez <vicomarquezmuniz@gmail.com>
2022-10-05 05:40:20 -0700

    task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,18 @@ class Rectangle(Base):
         d = self.width
         e = self.height
         return ("[Rectangle] ({:d}) {}/{} - {:d}/{:d}".format(a, b, c, d, e))
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
