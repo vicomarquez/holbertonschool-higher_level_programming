ab2494f55f942dff609d2b6c0193e5ff18236855
vicomarquez <vicomarquezmuniz@gmail.com>
2022-10-06 08:54:40 -0700

    test 1

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""rectangle tests"""
+
+
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+class TestRect(unittest.TestCase):
+    def test_rectangle(self):
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.id, 1)
