de81e2185eef067c7e0116c2645ed70e74a47f8a
vicomarquez <vicomarquezmuniz@gmail.com>
2022-10-06 09:08:50 -0700

    fix task

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,6 +7,11 @@ from models.rectangle import Rectangle
 from models.base import Base
 
 class TestRect(unittest.TestCase):
+    """rectangle tests"""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
     def test_rectangle(self):
         r1 = Rectangle(1, 2)
         self.assertEqual(r1.id, 1)
