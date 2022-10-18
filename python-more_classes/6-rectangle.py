74ce0c3e37bf0aafc45a055d4f2cce703124d2b6
vicomarquez <vicomarquezmuniz@gmail.com>
2022-10-18 13:15:46 -0300

    fix pep8

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     class Rectangle that defines a rectangle
     """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """init rectangle"""
         if type(width) != int:
