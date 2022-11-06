63e71027f88b92e492ca480f4139d1f864bc87ee
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-06 15:01:33 -0800

    fix 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Task 10
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genre INNER JOIN tv_shows ON tv_show_genre.show_id = tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genre INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
