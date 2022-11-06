5f346e488628671aa046655f4aa1640ce516b5aa
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-06 15:59:39 -0800

    fix task

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Task 13
-SELECT name AS 'genre', COUNT(*) AS 'number_of_shows' FROM tv_show_genres, tv_genres WHERE tv_genre.id = tv_show_genres.genre_id
+SELECT name AS 'genre', COUNT(*) AS 'number_of_shows' FROM tv_show_genres, tv_genres WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
