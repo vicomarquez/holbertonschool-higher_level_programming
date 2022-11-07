d473ee435bb8ae68b76d2385e372a8a7b66d1c85
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-06 16:45:56 -0800

    task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,6 @@
 -- Task 15
-SELECT name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Comedy'
-ORDER BY name ASC;
+SELECT title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title ASC;
