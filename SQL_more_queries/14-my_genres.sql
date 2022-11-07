a5203ce5d8994492ad69e913a6ac65efecc0dfa8
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-06 16:29:46 -0800

    fix 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
 -- Task 14
-SELECT name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.id
+SELECT name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
