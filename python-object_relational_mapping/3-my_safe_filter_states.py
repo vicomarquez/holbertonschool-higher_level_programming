6d0082150bf5447ec952c5f5d58ad3e6faf31703
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-13 14:57:03 -0800

    fix task

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     cur = connector.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name=%s", (argv[4]))
+    cur.execute("SELECT * FROM states WHERE name=%s", (argv[4], ))
 
     for row in cur.fetchall():
         print(row)
