048f6c153e160ad423ee92e0c4960ee997df77c1
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-13 14:48:01 -0800

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""  takes in arguments and displays all values in states table """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connector = MySQLdb.connect(host="localhost",
+                                port=3306,
+                                user=argv[1],
+                                passwd=argv[2],
+                                database=argv[3])
+
+    cur = connector.cursor()
+
+    cur.execute(
+            "SELECT * FROM states WHERE NAME = %s"
+            .format(argv[4]))
+
+    for row in cur.fetchall():
+        print(row)
