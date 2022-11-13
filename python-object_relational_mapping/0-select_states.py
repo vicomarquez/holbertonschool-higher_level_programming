5158b13f1c6d74dd780c3757256f70dc3a18b746
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-13 08:48:43 -0800

    fix pep8

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@ from sys import argv
 
 if __name__ == "__main__":
     connector = MySQLdb.connect(host="localhost",
-            port=3306, 
-            user=argv[1],
-            passwd=argv[2],
-            database=argv[3])
+                                port=3306,
+                                user=argv[1],
+                                passwd=argv[2],
+                                database=argv[3])
 
     cur = connector.cursor()
 
