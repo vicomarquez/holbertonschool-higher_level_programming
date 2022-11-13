6149dfcd593e39559688fb1625232ed600179d3e
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-13 15:39:10 -0800

    fix syntaxis

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
 
     cur = connector.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-            "LEFT JOIN states ON cities.state_id = state_id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
+                "states ON cities.state_id = states.id")
 
     for row in cur.fetchall():
         print(row)
