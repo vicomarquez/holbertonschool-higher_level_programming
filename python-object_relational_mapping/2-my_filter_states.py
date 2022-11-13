86b4a44d3f7a3fb915921b0cdb4473615d2057b8
vicomarquez <vicomarquezmuniz@gmail.com>
2022-11-13 09:42:18 -0800

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connector = MySQLdb.connect(host="localhost",
+                                port=3306,
+                                user=argv[1],
+                                passwd=argv[2],
+                                database=argv[3])
+
+    cur = connector.cursor()
+
+    cur.execute("SELECT * FROM states WHERE NAME='{}'".format(argv[4]))
+
+    for row in cur.fetchall():
+        print(row)
