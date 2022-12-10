1e127b63760dc8fec5c0f3670b32b72a49a9d3b9
vicomarquez <vicomarquezmuniz@gmail.com>
2022-12-10 15:33:02 -0800

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const number = parseInt(process.argv[2]);
+
+function factorial (number) {
+  if (!number) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+console.log(factorial(number));
