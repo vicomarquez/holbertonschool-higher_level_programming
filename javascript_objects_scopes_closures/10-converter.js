a127f34ac8dc3ec913d9e8fc5167cc7129841290
vicomarquez <vicomarquezmuniz@gmail.com>
2022-12-11 18:47:57 -0800

    fix semistandard

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-    return function (number) {
-      return parseInt(number, 10).toString(base);
-    };
+  return function (number) {
+    return parseInt(number, 10).toString(base);
   };
+};
