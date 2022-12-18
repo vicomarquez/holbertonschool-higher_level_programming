0e9e9d6cf693f1558aafb17a0f8d121ffd3a593a
vicomarquez <vicomarquezmuniz@gmail.com>
2022-12-17 18:07:45 -0800

    task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// The first argument is the movie ID
+// The status code must be printed like this: code: <status code>
+
+const request = require('request');
+let counter = 0;
+
+request(process.argv[2], function (error, resp, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const dt = JSON.parse(body);
+    for (let i = 0; dt.results[i] !== undefined; i++) {
+      if (dt.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        counter++;
+      }
+    }
+  }
+  console.log(counter);
+});
